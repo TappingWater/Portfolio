af8d803c61b1b7f406daf9f404c9ea20d51883ef
TappingWater <chanakaperera301@gmail.com>
2023-02-07 14:45:12 +0530

    added tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,15 @@
 import './globals.css'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+export default function RootLayout({children, title}: {
+  children: React.ReactNode,
+  title: string
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+      <head title={title}/>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
